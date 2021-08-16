b01edb950752530978797c55dd6595f7fa9185c8
Carolyn Chen <carolyn@NCS-Carolyn-MacBookPro.local>
2021-08-16 15:00:29 +0800

    folder structure

---
+++ src/tf/run_processor.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+cd ../../
+
+param0=""
+param1=""
+param2=""
+param3=""
+param4=""
+param5=""
+param6=""
+param7=""
+param8=""
+param9=""
+
+python -m src.cnn.main  --param0 ${param0} \
+--param1 ${param1} \
+--param2 ${param2} \
+--param3 ${param3} \
+--param4 ${param4} \
+--param5 ${param5} \
+--param6 ${param6} \
+--param7 ${param7} \
+--param8 ${param8} \
+--param9 ${param9} \
---
+++ src/transformer/train.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# -*- author: Carolyn Chen <yanchongcarolynchen@gmail.com> -*-
